4b7307d84171126ece64f400d055acf4958286fe
Aashish Rana <aashishrana71135@gmail.com>
2022-01-29 21:55:39 +0530

    setting controllers and router for the sign in and sign up pages

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,4 +1,6 @@
 module.exports.home = function(req, res){
+    console.log(req.cookies);  //if i put a semicolon it shows me the error
+    res.cookie('user_id', 25); // this is also showing me the error
 
     return res.render('home', {
         title: "Home"
