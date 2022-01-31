8f1491be473fefa17d11813a485fb83e0e9ef0aa
Aashish Rana <aashishrana71135@gmail.com>
2022-01-31 13:23:52 +0530

    setting passport library and try to send data

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const router = express.Router();
 
+const passport = require('passport');
+
 const usersController = require('../controllers/users_controller');
 
 router.get('/profile', usersController.profile);
@@ -11,4 +13,10 @@ router.get('/sign-in', usersController.signIn);
 
 router.post('/create', usersController.create);
 
+
+router.post('/create-session', passport.authenticate(
+    'local',
+ 
+    {failureRedirect: '/users/sign-in'}
+), usersController.createSession);
 module.exports = router;
